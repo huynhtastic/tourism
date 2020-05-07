5f84af6a592cdfe879e40808a2e9c13fe823ee53
Richard Huynh <huynhtastic@gmail.com>
2020-05-07 13:49:20 -0500

    Initial Commit

---
+++ lib/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tourism',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Tourism'),
+        ),
+      ),
+    );
+  }
+}
